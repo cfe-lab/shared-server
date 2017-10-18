cd61b3c83cb6c47b430269e9d19feb38e9d14672
Nathaniel Knight <nknight@cfenet.ubc.ca>
2017-10-18 10:52:08 -0700

    Add draft content to for the SHARED front page

--- shared_server/urls.py
+++ shared_server/urls.py
@@ -1,26 +1,40 @@
-"""shared_server URL Configuration
+"""shared_server URL configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic.base import TemplateView
 
+
+def static_page(name, path=None):
+    '''Render a static extension of the base template
+
+    This function creates a url and a View for a mostly static
+    template with the titles and navigation elements from
+    `base.html`. It's for rendering things like "About" and "Contact"
+    pages without duplicating elements from the site template.
+    '''
+    if path is None:
+        path = "^{}$".format(name)
+    url_name = name
+    template_name = name + ".html"
+    return url(
+        path,
+        TemplateView.as_view(template_name=template_name),
+        name=url_name,
+    )
+
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^datasubmission/', include('datasubmission.urls')),
-    url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
+    static_page('index', path=r'^$'),
+    static_page('about'),
+    static_page('contact'),
+    static_page('contributing'),
     url(
         r'^data_submission/',
         TemplateView.as_view(template_name='data_submission.html'),
