064fa80a7b4ddc69da056340970f514c304eedb2
Nat Knight <nathaniel.ep@gmail.com>
2018-12-31 14:26:49 -0800

    Add WhiteNoise to serve static files
    This commit adds the [WhiteNoise][wn] plugin to serve static files from
    the app server. This removes a step of the deployment process. The
    tradeoff is heavier load on the Python process that's running the
    server, but we're unlikely to ever be in that situation, so I think that
    the simplified deployment is very much the right choice for this
    project.
    
    wn: http://whitenoise.evans.io/en/stable/

--- shared_server/settings.py
+++ shared_server/settings.py
@@ -67,6 +67,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -139,11 +140,8 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-STATIC_ROOT = os.path.join(BASE_DIR, 'to_webroot')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
 
 # Publication urls
 SUBMISSION_EXAMPLE_PAGE = ('https://github.com/hcv-shared/'
